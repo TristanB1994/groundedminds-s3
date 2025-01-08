b20f674057807a86685dc956f2e8cbc27fd05feb
TristanB1994 <t.blandford.dev@gmail.com>
2025-01-08 17:57:02 -0500

    update origin

--- terraform/modules/s3/outputs.tf
+++ terraform/modules/s3/outputs.tf
@@ -20,6 +20,10 @@ output "bucket_rest_endpoint" {
   value = aws_s3_bucket.website_bucket.bucket_regional_domain_name
 }
 
+output "bucket_website_endpoint" {
+  value = aws_s3_bucket.website_bucket.website_endpoint
+}
+
 output "origin_access_identity_path" {
   value = aws_cloudfront_origin_access_identity.oai.cloudfront_access_identity_path
 }
