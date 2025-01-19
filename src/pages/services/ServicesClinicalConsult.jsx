a34eff31a455a4cb7a719fa41f8d72468c653159
TristanB1994 <t.blandford.dev@gmail.com>
2025-01-18 22:20:44 -0500

    rafeya

--- src/pages/services/ServicesClinicalConsult.jsx
+++ src/pages/services/ServicesClinicalConsult.jsx
@@ -34,53 +34,16 @@ const ServicesClinicalConsult = () => {
                 <section className="content">        
                     <div className="team_thumbnails">            
                         <div className="team_portrait">                
-                            <img alt="angie blandford therapist" id="ALB-CONSULTATION-THUMB" src="/ang_thumbnail.jpeg"/>        
-                                <button className="teamthumbs" id="ALB-CONSULTATION-CTA">                    
+                            <img alt="angie blandford therapist" id="AK-SECTION-THUMB" src="/aboutanka.jpeg"/>        
+                                <button className="teamthumbs" id="AK-SECTION-CTA">                    
                                     View Anka's Bio               
                                 </button>           
                         </div>        
                     </div>
                     <br/><br/>   
                 </section>    
-                <section className="content">    
-                    <p className="question" id="ALB-CONSULTATION-TARGET">      
-                        Angie Blandford, MSW, RSW
-                        <br/>
-                        (she/her)
-                    </p>
-                    <p className="pb10 aaa">    
-                        Angie Blandford (MSW, RSW) is the founder of Grounded Minds. Angie started Grounded Minds after identifying a growing need for specialized anxiety and OCD treatment. In addition to her clinical practice, Angie is available to support therapists who are doing this important work with their clients.
-                        <br/><br/> Angie offers virtual clinical consultation to fellow therapists who are looking for support implementing ERP for OCD or CBT for anxiety disorders. Angie has been formally trained in CBT for depression, generalized anxiety disorder, panic disorder, agoraphobia, specific fears and phobias, OCD, and PTSD. She has specialized training in ERP for OCD and CPT for PTSD. She spent several years offering structured psychotherapy to adults in Simcoe Muskoka prior to founding Grounded Minds.   
-                    </p>    
-                </section>    
-                <section className="content">        
-                    <p className="question">             
-                        Consultation topics may include:        
-                    </p>
-                    <div className="pb10 specialtiesli">          
-                        <ul className="aaa aaaL">               
-                            <li>CBT protocols for various anxiety disorders</li>   
-                            <li>CBT for OCD</li>          
-                            <li>ERP rationale</li>          
-                            <li>Supporting friends or family members of folks with OCD</li>   
-                            <li>Implementing CBT interventions</li>           
-                            <li>Implementing ERP for OCD</li>             
-                            <li>Identifying exposure exercises</li>       
-                            <li>Trouble shooting and problem solving</li>      
-                            <li>Private practice questions</li>      
-                            <li>Managing client motivation</li>   
-                            <li>Case conceptualization</li>   
-                        </ul>
-                    </div>        
-                    <br/><br/>
-                    <a className="booknow" href="mailto: anka@groundedminds.ca">     
-                        BOOK WITH ANKA        
-                    </a>
-                    <br/><br/>    
-                </section>
-
-                {/* Angie Consultation Section */}
-                <section className="content">        
+                  {/* Angie Consultation Section */}
+                  <section className="content">        
                     <div className="team_thumbnails">            
                         <div className="team_portrait">                
                             <img alt="angie blandford therapist" id="ALB-CONSULTATION-THUMB" src="/ang_thumbnail.jpeg"/>        
@@ -91,17 +54,6 @@ const ServicesClinicalConsult = () => {
                     </div>
                     <br/><br/>   
                 </section>    
-                <section className="content">    
-                    <p className="question" id="ALB-CONSULTATION-TARGET">      
-                        Angie Blandford, MSW, RSW
-                        <br/>
-                        (she/her)
-                    </p>
-                    <p className="pb10 aaa">    
-                        Angie Blandford (MSW, RSW) is the founder of Grounded Minds. Angie started Grounded Minds after identifying a growing need for specialized anxiety and OCD treatment. In addition to her clinical practice, Angie is available to support therapists who are doing this important work with their clients.
-                        <br/><br/> Angie offers virtual clinical consultation to fellow therapists who are looking for support implementing ERP for OCD or CBT for anxiety disorders. Angie has been formally trained in CBT for depression, generalized anxiety disorder, panic disorder, agoraphobia, specific fears and phobias, OCD, and PTSD. She has specialized training in ERP for OCD and CPT for PTSD. She spent several years offering structured psychotherapy to adults in Simcoe Muskoka prior to founding Grounded Minds.   
-                    </p>    
-                </section>    
                 <section className="content">        
                     <p className="question">             
                         Consultation topics may include:        
@@ -114,21 +66,16 @@ const ServicesClinicalConsult = () => {
                             <li>Supporting friends or family members of folks with OCD</li>   
                             <li>Implementing CBT interventions</li>           
                             <li>Implementing ERP for OCD</li>             
-                            <li>Identifying exposure exercises</li>       
+                            <li>Identifying exposure exercises</li>     
+                            <li>Treating BFRBs</li>  
                             <li>Trouble shooting and problem solving</li>      
                             <li>Private practice questions</li>      
                             <li>Managing client motivation</li>   
                             <li>Case conceptualization</li>   
                         </ul>
-                    </div>        
-                    <br/><br/>
-                    <a className="booknow" href="mailto: angie@groundedminds.ca">     
-                        BOOK WITH ANGIE        
-                    </a>
-                    <br/><br/>    
-                </section>    
-                <section className="content">        
-                    <br/><br/><br/><br/>        
+                    </div>     
+                    <br></br>   <br></br>
+                    <section className="content">              
                     <div className="serviceimg">
                         <img alt="Online therapy setting" src="/consultation.jpeg"/>   
                         <div className="sidesplt">
@@ -138,6 +85,41 @@ const ServicesClinicalConsult = () => {
                         <br/><br/><br/><br/>    
                     </div>
                 </section>
+                    <section className="content">    
+                    <p className="question" id="ALB-CONSULTATION-TARGET">      
+                        Anka Krivokuca, MSW, RSW
+                        <br/>
+                        (she/her)
+                    </p>
+                    <p className="pb10 aaa">    
+                        Anka Krivokuca is a Registered Social Worker with her Masters in Social Work. Anka has many years of experience treating anxiety disorders, obsessive compulsive disorder, body dysmorphia, body-focused repetitive behaviours and depression. She also works with trauma and PTSD. Her primary therapeutic modalities include CBT, ERP, CPT and PE. Anka works full time offering structured CBT for anxiety disorders, depression, OCD and PTSD. She also works in private practice offering therapy and consultation services.
+                        <br/><br/> Anka offers virtual clinical consultation to fellow therapists who are looking for support implementing ERP for OCD or CBT for anxiety disorders. She can also offer support when treating BFRBs or body dysmorphia. Anka has been formally trained in CBT for depression, generalized anxiety disorder, panic disorder, agoraphobia, specific fears and phobias, OCD, and PTSD. She has specialized training in ERP for OCD and CPT and PE for PTSD.   
+                    </p>    
+                </section>   
+                <br></br> <br></br>
+                    <a className="booknow" href="https://groundedmindstherapy.janeapp.com/#/staff_member/7">     
+                        BOOK WITH ANKA        
+                    </a>
+                    <br/><br/>    
+                </section>
+                <br></br>
+                <section className="content">    
+                    <p className="question" id="ALB-CONSULTATION-TARGET">      
+                        Angie Blandford, MSW, RSW
+                        <br/>
+                        (she/her)
+                    </p>
+                    <p className="pb10 aaa">    
+                        Angie Blandford is the founder of Grounded Minds. Angie started Grounded Minds after identifying a growing need for specialized anxiety and OCD treatment. In addition to her clinical practice, Angie is available to support therapists who are doing this important work with their clients.
+                        <br/><br/> Angie offers virtual clinical consultation to fellow therapists who are looking for support implementing ERP for OCD or CBT for anxiety disorders. Angie has been formally trained in CBT for depression, generalized anxiety disorder, panic disorder, agoraphobia, specific fears and phobias, OCD, and PTSD. She has specialized training in ERP for OCD and CPT for PTSD. She spent several years offering structured psychotherapy to adults in Simcoe Muskoka prior to founding Grounded Minds.   
+                    </p>    
+                </section>    
+                <section className="content">        
+                    <br/><br/>
+                    <a className="booknow" href="https://groundedmindstherapy.janeapp.com/#/staff_member/1">     
+                        BOOK WITH ANGIE        
+                    </a> 
+                </section>    
             </Layout>
         </>
     );
