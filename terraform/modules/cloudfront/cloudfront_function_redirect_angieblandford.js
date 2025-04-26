36a294e977de4247c1bd8a1f96973164dd683c2b
TristanB1994 <t.blandford.dev@gmail.com>
2025-04-26 14:30:05 -0400

    extend redirection

--- terraform/modules/cloudfront/cloudfront_function_redirect_angieblandford.js
+++ terraform/modules/cloudfront/cloudfront_function_redirect_angieblandford.js
@@ -1,14 +1,27 @@
-// CloudFront Function: Redirect /angieblandford to /about/angieblandford on the correct domain
-// TODO: make the request.uri a variable and switch case it for all redirects
+// CloudFront Function: Redirect specific root paths to /about/NAME on the correct domain
 function handler(event) {
     var request = event.request;
     var host = request.headers.host.value;
-    if (request.uri === "/angieblandford" || request.uri === "/angieblandford/") {
+    var redirects = {
+        "/angieblandford": "/about/angieblandford",
+        "/angieblandford/": "/about/angieblandford",
+        "/ankakrivokuca": "/about/ankakrivokuca",
+        "/ankakrivokuca/": "/about/ankakrivokuca",
+        "/daniellekirshenbaum": "/about/daniellekirshenbaum",
+        "/daniellekirshenbaum/": "/about/daniellekirshenbaum",
+        "/liannesaxon": "/about/liannesaxon",
+        "/liannesaxon/": "/about/liannesaxon",
+        "/moonsunjang": "/about/moonsunjang",
+        "/moonsunjang/": "/about/moonsunjang",
+        "/rafeyashami": "/about/rafeyashami",
+        "/rafeyashami/": "/about/rafeyashami"
+    };
+    if (redirects[request.uri]) {
         return {
             statusCode: 301,
             statusDescription: "Moved Permanently",
             headers: {
-                "location": { "value": "https://" + host + "/about/angieblandford" }
+                "location": { "value": "https://" + host + redirects[request.uri] }
             }
         };
     }
